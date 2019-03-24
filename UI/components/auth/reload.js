02382d83d1a15c2e873170ce825ff6d8f24cdceb
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-03-24 20:27:38 +0300

    feat (spa) single page app  - change app into a single page app  - retructure the app like a react ap
    [Finishes #164682832]

---
+++ UI/components/auth/reload.js
@@ -0,0 +1,9 @@
+// import { LForm } from './loginValidations';
+// window.onload=function(){
+//     if(location.hash === '#/' || location.hash === ''){
+//         window.loginForm = document.getElementById('loginForm');
+//         window.loginForm.addEventListener('submit', LForm);
+//     }
+//     // window.loginForm = document.getElementById('loginForm');
+//     // window.loginForm.addEventListener('submit', LForm);
+//   }
